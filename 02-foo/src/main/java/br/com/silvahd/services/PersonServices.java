696b15e5728a47202719b75cd271bad9ff0dc091
Diego Henrique Silva <diegosilvadhs@gmail.com>
2022-09-26 19:35:10 -0300

    CRUD

--- 02-foo/src/main/java/br/com/silvahd/services/PersonServices.java
+++ 02-foo/src/main/java/br/com/silvahd/services/PersonServices.java
@@ -1,5 +1,8 @@
 package br.com.silvahd.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -11,12 +14,43 @@ public class PersonServices {
 	
 	private final AtomicLong couter = new AtomicLong();
 	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete(String id) {
+	}
+	
+	
 	public Person findById(String id) {
 		Person person = new Person();
 		person.setId(couter.incrementAndGet());
 	    person.setFistName("Diego");
 	    person.setLastName("Henrique");
 	    person.setAddress("Uberlândia - Minas Gerais - Brasil");
+	    person.setGender("Male");
+		return person;
+	}
+	
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(couter.incrementAndGet());
+	    person.setFistName("Person name" + i);
+	    person.setLastName("Last name" + i);
+	    person.setAddress("Some address - Brasil" + i);
 	    person.setGender("Male");
 		return person;
 	}
